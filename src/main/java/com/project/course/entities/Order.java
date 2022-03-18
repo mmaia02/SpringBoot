d96553f0329a65a016f1d4f2bcab0ca7571814cb
mmaia02 <mauriciomaiacv@gmail.com>
2022-03-17 21:51:54 -0300

    Entidade OrderItem. Associação many-to-many com dados extras

--- src/main/java/com/project/course/entities/Order.java
+++ src/main/java/com/project/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.project.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -33,8 +36,10 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
-		
 	}
 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -78,6 +83,10 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
